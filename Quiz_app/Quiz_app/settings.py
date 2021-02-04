231de01588e316fd6979ca77372c4b1323b942eb
Mayu008 <kadam.ms@somaiya.edu>
2021-02-05 03:06:52 +0530

    hosting site

--- Quiz_app/Quiz_app/settings.py
+++ Quiz_app/Quiz_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
@@ -129,3 +131,9 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL='blog.home'
 LOGIN_URL='login'
+
+
+
+#Activate Django-Heroku
+
+django_heroku.settings(locals())
